17f4e6ed8594ca6816c81b96145a2796125b088e
Pedram Sheikhrahimi <pedr012G7sh@gmail.com>
2022-05-19 06:21:32 -0700

    feat: add different reactions to inputs
    This sets different responses to equal, lower or higher inputs.

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 'use strict';
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
 document.querySelector('.check').addEventListener('click', () => {
-    const guess = document.querySelector('.guess').value;
+    const guess = Number(document.querySelector('.guess').value);
 
     if(!guess)
         document.querySelector('.message').textContent = '⚠️ No number!';
+    else if(guess === secretNumber)
+        document.querySelector('.message').textContent = '🎉 Correct number!';
+    else if(guess < secretNumber)
+        document.querySelector('.message').textContent = '📉 Too low';
+    else if(guess > secretNumber)
+        document.querySelector('.message').textContent = '📈 Too high';
 });
