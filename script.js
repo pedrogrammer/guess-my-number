f7874db70a5f793eb433a013b95b874da79ad6a9
pedramsheikhrahimi <pedr012G7sh@gmail.com>
2022-05-21 02:09:07 -0700

    feat: add visual reactions to the correct number
    - background-color
    - secret number reveal and its container width

--- script.js
+++ script.js
@@ -7,10 +7,17 @@ let score = 20;
 document.querySelector('.check').addEventListener('click', () => {
     const guess = Number(document.querySelector('.guess').value);
 
+    // When there is no input
     if(!guess)
         document.querySelector('.message').textContent = '⚠️ No number!';
-    else if(guess === secretNumber)
+    // When a player wins
+    else if(guess === secretNumber) {
         document.querySelector('.message').textContent = '🎉 Correct number!';
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+        document.querySelector('.number').textContent = secretNumber;
+    }
+    // When a guess is too high
     else if(guess < secretNumber) {
         if(score > 1) {
             document.querySelector('.message').textContent = '📉 Too low';
@@ -20,7 +27,9 @@ document.querySelector('.check').addEventListener('click', () => {
             document.querySelector('.message').textContent = '☹️ You lost!';
             document.querySelector('.score').textContent = 0;
         }
-    } else if(guess > secretNumber) {
+    }
+    // When a guess is too low
+    else if(guess > secretNumber) {
         if(score > 1) {
             document.querySelector('.message').textContent = '📈 Too high';
             score--;
