209c43e734dcff54c6a77020c83fbc8b336ae5df
pedramsheikhrahimi <pedr012G7sh@gmail.com>
2022-05-21 22:30:14 -0700

    refactor: make code cleaner
    This removes repetitive code and enhances naming.

--- script.js
+++ script.js
@@ -1,59 +1,65 @@
 'use strict';
 
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
-let highScore = 0;
+const
+    displayMessage = message => document.querySelector('.message').textContent = message,
+    bodyBackgroundColor = color => document.querySelector('body').style.backgroundColor = color,
+    secretNumberContainerWidth = width => document.querySelector('.secret-number').style.width = width,
+    secretNumberValue = () => Math.trunc(Math.random() * 20) + 1,
+    displaySecretNumber = secretNumber => document.querySelector('.secret-number').textContent = secretNumber,
+    displayScore = score => document.querySelector('.score').textContent = score,
+    guessValue = guess => document.querySelector('.guess').value = guess
+;
+
+let 
+    secretNumber = secretNumberValue(),
+    score = 20,
+    highScore = 0
+;
 
 document.querySelector('.check').addEventListener('click', () => {
     const guess = Number(document.querySelector('.guess').value);
 
     // When there is no input
     if(!guess)
-        document.querySelector('.message').textContent = '⚠️ No number!';
+        displayMessage('⚠️ No number!');
     // When a player wins
     else if(guess === secretNumber) {
-        document.querySelector('.message').textContent = '🎉 Correct number!';
-        document.querySelector('body').style.backgroundColor = '#60b347';
-        document.querySelector('.number').style.width = '30rem';
-        document.querySelector('.number').textContent = secretNumber;
+        displayMessage('🎉 Correct number!');
+        bodyBackgroundColor('#60b347');
+        secretNumberContainerWidth('30rem');
+        displaySecretNumber(secretNumber);
 
         if(score > highScore) {
             highScore = score;
             document.querySelector('.high-score').textContent = highScore;
         }
     }
-    // When a guess is too high
-    else if(guess < secretNumber) {
+    // When a guess is wrong
+    else if(guess !== secretNumber) {
         if(score > 1) {
-            document.querySelector('.message').textContent = '📉 Too low';
-            score--;
-            document.querySelector('.score').textContent = score;
+            if(score > 1) {
+                displayMessage(guess > secretNumber ? '📈 Too high' : '📉 Too low');
+                score--;
+                displayScore(score);
+            } else {
+                displayMessage('☹️ You lost!');
+                displayScore(0);
+            }    
         } else {
-            document.querySelector('.message').textContent = '☹️ You lost!';
-            document.querySelector('.score').textContent = 0;
-        }
-    }
-    // When a guess is too low
-    else if(guess > secretNumber) {
-        if(score > 1) {
-            document.querySelector('.message').textContent = '📈 Too high';
-            score--;
-            document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = '☹️ You lost!';
-            document.querySelector('.score').textContent = 0;
+            displayMessage('☹️ You lost!');
+            displayScore(0);
         }
     }
 });
 
 document.querySelector('.again').addEventListener('click', () => {
     score = 20;
-    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    secretNumber = secretNumberValue();
 
-    document.querySelector('.guess').value = '';
-    document.querySelector('.number').textContent = '?';
-    document.querySelector('.message').textContent = 'Start guessing ...';
-    document.querySelector('.score').textContent = score;
-    document.querySelector('body').style.backgroundColor = '#222';
-    document.querySelector('.number').style.width = '15rem';
+    guessValue('');
+    displaySecretNumber('?');
+    displayMessage('Start guessing ...');
+    displayScore(score);
+    bodyBackgroundColor('#222');
+    secretNumberContainerWidth('15rem');
 });
